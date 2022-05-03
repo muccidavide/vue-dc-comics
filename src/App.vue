5cb866cc53a97d70dd6995be48d8d35195308171
muccidavide <davidemucci0804@gmail.com>
2022-05-03 15:23:19 +0200

    Add header Component

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <HeaderSite/>
+      
+    <main id="main_site">
+
+    </main>
+    <footer id="footer_site">
+
+    </footer>
+
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import HeaderSite from '@/components/HeaderComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderSite,
+  },
+      
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 1rem;
+  font-family: 'Montserrat', sans-serif;
+}
+
+/* Reset */
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+img{
+  max-width: 100%;
+}
+
+.wrapper{
+  width: 80%;
+  max-width: 1440px;
+  margin: auto;
+}
+
+.row{
+  display: flex;
+  flex-wrap:nowrap;
+  justify-content: space-between;
+  align-items: center;
+}
+
+/* Header */
+header{
+  .site_logo img{
+    width: 80px;
+  }
+  nav{
+    background-color: white;
+    height: 80px;
+          display: flex;
+      align-items: center;
+    ul{
+      list-style: none;
+
+    }
+    li{
+      font-size: 0.8rem;
+      padding: 0 1rem;
+      font-weight: bold;
+      text-transform: uppercase;
+    }
+  }
+}
+
+
+
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <header id="header_site" class="row">
+      <div class="site_logo">
+        <img src="@/assets/img/dc-logo.png" alt="logo DC">
+      </div>
+      <nav class="nav_site">
+        <ul class="row">
+          <li v-for="item in navItems" :key="item" class="col">{{item}}</li>
+        </ul>
+
+
+      </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderSite',
+    data(){
+      return{
+        navItems:['characters',
+        'comics',
+        'movies',
+        'tv',
+        'games',
+        'collectibles',
+        'videos',
+        'fans',
+        'news',
+        'shop']
+      }
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
