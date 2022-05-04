dbeae3add2d1389b525deff18fcd7cad63ef571a
muccidavide <davidemucci0804@gmail.com>
2022-05-04 14:10:13 +0200

    Complete without Component

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,29 @@
       <HeaderSite />
 
       <main id="main_site">
-        <div class="products"></div>
+        <div class="products">
+          <div class="container">
+            <div class="row">
+              <div class="col-2" v-for="card in cards" :key="card.thumb">
+                <div class="img_box">
+                  <img :src="card.thumb" :alt="card.type" />
+                </div>
+                <h5>{{ card.series }}</h5>
+              </div>
+            </div>
+            <div>
+              <button>Load More</button>
+            </div>
+          </div>
+        </div>
         <MainNav></MainNav>
       </main>
       <FooterSite></FooterSite>
-
     </div>
   </div>
 </template>
 
 <script>
-
 import HeaderSite from "@/components/HeaderComponent.vue";
 import MainNav from "@/components/MainNavComponent.vue";
 import FooterSite from "@/components/FooterComponent.vue";
@@ -25,17 +37,132 @@ export default {
     MainNav,
     FooterSite,
   },
-
+  data() {
+    return {
+      cards: [
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+          price: "$19.99",
+          series: "Action Comics",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+          price: "$3.99",
+          series: "American Vampire 1976",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+          price: "$16.99",
+          series: "Aquaman",
+          type: "graphic novel",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+          price: "$2.99",
+          series: "Batgirl",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+          price: "$3.99",
+          series: "Batman",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+          price: "$2.99",
+          series: "Batman Beyond",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+          price: "$3.99",
+          series: "Batman/Superman",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+          price: "$4.99",
+          series: "Batman/Superman Annual",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+          price: "$5.99",
+          series: "Batman: The Joker War Zone",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+          price: "$6.99",
+          series: "Batman: Three Jokers",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+          price: "$4.99",
+          series: "Batman: White Knight Presents: Harley Quinn",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+          price: "$16.99",
+          series: "Catwoman",
+          type: "graphic novel",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style lang="scss">
- @import '~@/assets/scss/style.scss'; 
+@import "~@/assets/scss/style.scss";
 
 .products {
-  height: 150px;
   background-color: $dc-dark;
+  .row {
+    flex-wrap: wrap;
+    .col-2 {
+      width: calc(100% / 6);
+      padding: 2rem 1rem;
+      text-align: left;
+
+      .img_box{
+      
+      aspect-ratio: 1/1;
+      overflow: hidden;
+      margin-bottom: 1rem;
+        img{
+          width: 100%;
+        }
+      }
+
+      h5 {
+        color: $dc-light;
+      }
+    }
+  }
+   button {
+        @include button;
+        background: $dc-primary;
+        padding: 0.5rem;
+        width: 200px;
+        margin-bottom: 1rem;
+      }
 }
-
-
 </style>
